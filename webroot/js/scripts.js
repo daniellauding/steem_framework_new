ae8a6005c40592561f1b531768101716a241c07e
Daniel Lauding <daniel@lauding.se>
2011-12-01 10:49:37 +0100

    asd

--- webroot/js/scripts.js
+++ webroot/js/scripts.js
@@ -1,3 +1,15 @@
 $(document).includeReady(function () {
 
+    /* TOP NAV */
+
+    var ul = $('.nav');
+
+    if (ul.length) {
+        ul.children('li').hover(function() {
+            $(this).children('ul').show();
+        }, function() {
+            $(this).children('ul').hide();
+        }).children('ul').hide();
+    }
+
 });
